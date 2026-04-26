bcdac3c6a4858dbfb759c0ca892275c12e15e839
Seungmin Yoo <msnoeuan@gmail.com>
2026-04-26 20:43:49 +0900

    refactor: 파일 기반 의도 분석 처리를 위한 IntentCode 클래스 제거

--- src/models/voice.py
+++ src/models/voice.py
@@ -1,4 +1,4 @@
-# voice_sessions / voice_commands / intent_codes / conversation_events
+# voice_sessions / voice_commands / conversation_events
 from src.core.database import Base  # Base import
 
 class VoiceSession(Base):
@@ -6,9 +6,6 @@ class VoiceSession(Base):
 
 class VoiceCommand(Base):
     __tablename__ = "voice_commands"
-
-class IntentCode(Base):
-    __tablename__ = "intent_codes"
 
 class ConversationEvent(Base):
     __tablename__ = "conversation_events"
