1a07afb4d0f7d72af37bc8fce542bce00d93fa9d
ka11y <elsword485667@gmail.com>
2026-05-03 02:54:03 +0900

    feat: CORS 허용

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from src.api.weather import router as weather_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_method=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 app.include_router(weather_router)
 
