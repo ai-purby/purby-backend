a2c10713ca549999cebe832e638f3dba4c7c4270
ka11y <elsword485667@gmail.com>
2026-05-04 21:41:15 +0900

    refactor: api prefix추가

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 from fastapi import APIRouter, FastAPI
+from src.api.weather import router as weather_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 api_router = APIRouter(prefix="/api")
 
@@ -11,3 +23,6 @@ def read_root():
 
 
 app.include_router(api_router)
+app.include_router(weather_router)
+
+
