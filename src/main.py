c0c538a3fa904f741114b2a38d009a6969d302b4
ka11y <elsword485667@gmail.com>
2026-05-04 20:56:47 +0900

    refactor: router 구조 변경

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+
+api_router = APIRouter(prefix="/api")
+
+
+@api_router.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+app.include_router(api_router)
